bf478acfa7bfaffe7a6924320e381083cd14cb3f
Mostafa-Usama <mostafa.usama286@gmail.com>
2024-09-15 12:36:23 +0300

    first commit

---
+++ Helpless/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    float moveX, moveZ;
+    
+    Vector3 direction = new Vector3();
+    public CharacterController cc;
+    public float moveSpeed = 10f;
+    void Start()
+    {
+        
+        
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveZ = Input.GetAxisRaw("Vertical");
+        direction = transform.right * moveX + transform.forward * moveZ;
+        cc.Move(direction * moveSpeed * Time.deltaTime);
+       
+    }
+  
+}

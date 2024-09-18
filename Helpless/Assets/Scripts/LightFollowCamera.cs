a74637cc9aa6bd5dcbbb40fd9c249680a2371f9c
Mostafa-Usama <mostafa.usama286@gmail.com>
2024-09-18 20:28:18 +0300

    basic flashlight

---
+++ Helpless/Assets/Scripts/LightFollowCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollowCamera : MonoBehaviour
+{
+    public Transform PlayerCamera;
+    public float speed = 3f;
+    Vector3 offset = Vector3.zero;
+    public Light FlashLight;
+    bool LightState = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - PlayerCamera.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerCamera.position - offset;
+        FlashLight.transform.rotation = Quaternion.Slerp(FlashLight.transform.rotation, PlayerCamera.rotation, speed* Time.deltaTime );
+        if (Input.GetKeyDown(KeyCode.E)){
+           FlashLight.enabled = !FlashLight.enabled;
+           LightState = !LightState;
+        }
+    }
+}
---
+++ Helpless/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    float moveX, moveZ;
+    public Transform CharacterModel;
+    
+    Vector3 direction = new Vector3();
+    public CharacterController cc;
+    public Animator playeranim;
+    public float wSpeed, wbSpeed, rnSpeed;
+    float oldwSpeed;
+    bool walking = false;
+    
+    private void Start()
+    {
+        oldwSpeed = wSpeed;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveZ = Input.GetAxisRaw("Vertical");
+        direction = transform.right * moveX + transform.forward * moveZ;
+        cc.Move(direction * wSpeed * Time.deltaTime);
+
+
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playeranim.SetTrigger("walk");
+            playeranim.ResetTrigger("idle");
+            walking = true;
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            playeranim.ResetTrigger("walk");
+            playeranim.SetTrigger("idle");
+            walking = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            
+            playeranim.SetTrigger("walkback");
+            playeranim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playeranim.ResetTrigger("walkback");
+            playeranim.SetTrigger("idle");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {   
+            playeranim.SetTrigger("walk");
+            playeranim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            playeranim.ResetTrigger("walk");
+            playeranim.SetTrigger("idle");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playeranim.SetTrigger("walk");
+            playeranim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            playeranim.ResetTrigger("walk");
+            playeranim.SetTrigger("idle");
+        }
+        if (walking)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                wSpeed = wSpeed + rnSpeed;
+                playeranim.ResetTrigger("walk");
+                playeranim.SetTrigger("sprint");
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                wSpeed = oldwSpeed ;
+                playeranim.ResetTrigger("sprint");
+                playeranim.SetTrigger("walk");
+            }
+        }
+    }
+  
+}
